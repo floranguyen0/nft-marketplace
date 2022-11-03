0ebd1a602192b925ff1d3d86669b0fbc6469c394
FloraNguyen <floranguyen010@gmail.com>
2022-11-03 19:11:16 +0700

    incorporate IAuction into Auction contract

--- src/Auction.sol
+++ src/Auction.sol
@@ -35,7 +35,7 @@ interface INFT {
 /// @notice Allows auctioning of ERC1155 NFTs in a first-price auction
 /// @dev Assumes the existence of a Registry as specified in IRegistry
 /// @dev Assumes an ERC2981-compliant NFT, as specified below
-contract Auction is IAuction, Ownable, ReentrancyGuard {
+contract Auction is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
     using Counters for Counters.Counter;
 
@@ -44,6 +44,39 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
 
     Counters.Counter private _auctionId;
     IRegistry private Registry;
+
+    event NewAuction(uint256 indexed auctionId, Auction newAuction);
+    event AuctionCancelled(uint256 indexed auctionId);
+    event BidPlaced(uint256 auctionId, uint256 amount);
+    event ClaimNFT(
+        uint256 auctionId,
+        address winner,
+        address recipient,
+        uint256 amount
+    );
+    event BalanceUpdated(
+        address indexed accountOf,
+        address indexed tokenAddress,
+        uint256 indexed newBalance
+    );
+
+    struct Auction {
+        uint256 id; // id of auction
+        address owner; // address of NFT owner
+        address nftContract;
+        uint256 nftId;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 reservePrice; // may need to be made private
+        address currency; // use zero address or 0xeee for ETH
+    }
+
+    struct Bid {
+        uint256 auctionId;
+        address bidder;
+        uint256 amount;
+        uint256 timestamp;
+    }
 
     mapping(uint256 => Auction) private auctions;
     mapping(uint256 => bool) private cancelled;
@@ -59,7 +92,9 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
         Registry = IRegistry(registry);
     }
 
-    /// @inheritdoc IAuction
+    /// @notice Returns a struct with an auction's details
+    /// @param auctionId the index of the auction being queried
+    /// @return an "Auction" struct with the details of the auction requested
     function getAuctionDetails(uint256 auctionId)
         external
         view
@@ -72,11 +107,13 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
         return auctions[auctionId];
     }
 
-    /// @inheritdoc IAuction
+    /// @notice Returns the status of a particular auction
+    /// @dev statuses are: PENDING, CANCELLED, ACTIVE, ENDED, ENDED & CLAIMED
+    /// @param auctionId the index of the auction being queried
+    /// @return a string of the auction's status
     function getAuctionStatus(uint256 auctionId)
         public
         view
-        override
         returns (string memory)
     {
         require(
@@ -95,7 +132,11 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
         revert("error");
     }
 
-    /// @inheritdoc IAuction
+    /// @notice Returns the in-contract balance of a specific address for a specific token
+    /// @dev use address(0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa) for ETH
+    /// @param account the address to query the balance of
+    /// @param token the address of the token to query the balance for
+    /// @return the uint256 balance of the token queired for the address queried
     function getClaimableBalance(address account, address token)
         external
         view
@@ -104,7 +145,11 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
         return claimableFunds[account][token];
     }
 
-    /// @inheritdoc IAuction
+    /// @notice Returns details of a specific bid
+    /// @dev the amount of an outbid bid is reduced to zero
+    /// @param auctionId the index of the auction the bid was places in
+    /// @param bidder the address of the bidder
+    /// @return a Bid struct with details of a specific bid
     function getBidDetails(uint256 auctionId, address bidder)
         external
         view
@@ -113,7 +158,9 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
         return bids[auctionId][bidder];
     }
 
-    /// @inheritdoc IAuction
+    /// @notice Returns the address of the current highest bidder in a particular auction
+    /// @param auctionId the index of the auction being queried
+    /// @return the address of the highest bidder
     function getHighestBidder(uint256 auctionId)
         external
         view
@@ -122,7 +169,16 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
         return highestBid[auctionId];
     }
 
-    /// @inheritdoc IAuction
+    /// @notice Creates a first-price auction for a ERC1155 NFT
+    /// @dev NFT contract must be ERC2981-compliant and recognized by Registry
+    /// @dev use address(0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa) for ETH
+    /// @param nftContract the address of the NFT contract
+    /// @param id the id of the NFT on the NFT contract
+    /// @param startTime uint256 timestamp when the auction should commence
+    /// @param endTime uint256 timestamp when auction should end
+    /// @param reservePrice minimum price for bids
+    /// @param currency address of the token bids should be made in
+    /// @return the index of the auction being created
     function createAuction(
         address nftContract,
         uint256 id,
@@ -172,7 +228,12 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
         return auctionId;
     }
 
-    /// @inheritdoc IAuction
+    /// @notice Allows bidding on a specifc auction
+    /// @dev use address(0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa) for ETH
+    /// @param auctionId the index of the auction to bid on
+    /// @param amountFromBalance the amount to bid from msg.sender's balance in this contract
+    /// @param externalFunds the amount to bid from funds in msg.sender's personal balance
+    /// @return a bool indicating success
     function bid(
         uint256 auctionId,
         uint256 amountFromBalance,
@@ -259,7 +320,13 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
         return true;
     }
 
-    /// @inheritdoc IAuction
+    /// @notice Allows the winner of the auction to claim their NFT
+    /// @notice Alternatively, allows auctioner to reclaim on an unsuccessful auction
+    /// @dev this function delivers the NFT and moves the bid to the auctioner's claimable balance
+    ///   and also accounts for the system fee and royalties (if applicable)
+    /// @param auctionId the index of the auction to bid on
+    /// @param recipient the address the NFT should be sent to
+    /// @return a bool indicating success
     function claimNft(uint256 auctionId, address recipient)
         external
         returns (bool)
@@ -323,7 +390,9 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
         return true;
     }
 
-    /// @inheritdoc IAuction
+    /// @notice Withdraws in-contract balance of a particular token
+    /// @dev use address(0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa) for ETH
+    /// @param tokenContract the address of the token to claim
     function claimFunds(address tokenContract) external {
         require(
             claimableFunds[msg.sender][tokenContract] > 0,
@@ -346,7 +415,9 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
         );
     }
 
-    /// @inheritdoc IAuction
+    /// @notice Allows contract owner to send NFT to auction winner and funds to auctioner's balance
+    /// @dev prevents assets from being stuck if winner does not claim
+    /// @param auctionId the index of the auction to resolve
     function resolveAuction(uint256 auctionId) external onlyOwner {
         require(
             keccak256(bytes(getAuctionStatus(auctionId))) ==
@@ -375,7 +446,8 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
         );
     }
 
-    /// @inheritdoc IAuction
+    /// @notice Allows contract owner or auctioner to cancel a pending or active auction
+    /// @param auctionId the index of the auction to cancel
     function cancelAuction(uint256 auctionId) external {
         require(
             msg.sender == auctions[auctionId].owner || msg.sender == owner(),
@@ -454,7 +526,7 @@ contract Auction is IAuction, Ownable, ReentrancyGuard {
         );
     }
 
-    /// @inheritdoc IAuction
+    /// @notice allows contract to receive ERC1155 NFTs
     function onERC1155Received(
         address operator,
         address from,
